a695c22e84d0e9d64c8dbe6ed0b80634d9700e7f
Olha <olha4229@gmail.com>
2025-08-02 21:07:48 +0300

    image-cards-added

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -6,6 +6,6 @@ const BASE_URL = 'https://pixabay.com/api/';
 export async function fetchImages(query) {
     const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&per_page=9`;
 
-    const data = await axios.get(url);
+    const response = await axios.get(url);
     return response.data.hits;
 }
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,11 +1,28 @@
 export function renderGallery(images, container) {
-    const markup = images
-        .map(
-            image => `
-        <div class="image-card">
-        <img src="${image.webformatURL}" alt="${image.tags}"/>
-        </div>`
-    ).join('');
+    const markup = images.map(image => {
+        const {
+            webformatURL,
+            largeImageURL,
+            tags,
+            likes,
+            views,
+            comments,
+            downloads,
+        } = image;
+
+        return `
+        <li class="gallery-item">
+        <a href="${largeImageURL}" class="gallery-link">
+        <img src="${webformatURL}" alt="${tags}" class="gallery-image"/>
+        </a>
+        <div class="info">
+        <p><b>Likes:</b>${likes}</p>
+        <p><b>Views:</b>${views}</p>
+        <p><bComments:b>${comments}</p>
+        <p><b>Downloads:</b>${downloads}</p>
+        </div>
+        </li>`;
+    }).join('');
     
-    container.innerHTML = markup;
+    container.innerHTML = `<ul class="gallery-list">${markup}</ul>`;
 }
